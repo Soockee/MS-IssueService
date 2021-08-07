8e660210aa2d11067a6f5454f48880ed415048a8
Björn Franke <bjoern.franke@mni.thm.de>
2021-08-07 09:38:14 +0200

    Secure issue-enpoints

--- src/issue/issue.controller.ts
+++ src/issue/issue.controller.ts
@@ -1,44 +1,52 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { IssueService } from './issue.service';
 import { CreateIssueDto } from './dto/create-issue.dto';
 import { UpdateIssueDto } from './dto/update-issue.dto';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller()
 export class IssueController {
   constructor(private readonly issueService: IssueService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('issue')
   create(@Body() createIssueDto: CreateIssueDto) {
     return this.issueService.create(createIssueDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('issues')
   findAll() {
     return this.issueService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('issue/:issueId')
   findOne(@Param('issueId') issueissueId: string) {
     return this.issueService.findOne(issueissueId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch('issue/:issueId')
   update(@Param('issueId') issueissueId: string, @Body() updateIssueDto: UpdateIssueDto) {
     return this.issueService.update(issueissueId, updateIssueDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete('issue/:issueId')
   remove(@Param('issueId') issueissueId: string) {
     return this.issueService.remove(issueissueId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('issue/:issueId/commment')
   createComment(@Param('issueId') issueId: string, @Body() creatCommentDto: CreateCommentDto) {
     return this.issueService.addComment(issueId, creatCommentDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('issue/:issueId/commments')
   findAllComments(@Param('issueId') issueId: string) {
     return this.issueService.findAllComments(issueId);
